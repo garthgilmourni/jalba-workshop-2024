31a625d55c8cc1f40d20788eba37f056c6fe5474
Garth Gilmour <garth.gilmour@jetbrains.com>
2024-05-16 17:50:08 +0100

    Add project for demoing Kotlin Dataframe

---
+++ notebook-dataframes/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+package data.frame.demos
+
+fun main() {
+    val fileName = "using-dataframes.ipynb"
+    val message = """
+        This project uses Kotlin Notebooks,
+        please see '$fileName'
+    """.trimIndent()
+
+    println(message)
+}
